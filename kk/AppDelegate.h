d003afdea60636310f2d2489115039a0db07724e
Sunil <Sunil@signitysolutions.com>
2014-02-22 09:28:21 +0530

    first

---
+++ kk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  kk
+//
+//  Created by Sunil Banjara on 22/02/14.
+//  Copyright (c) 2014 signity macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kk/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  kk
+//
+//  Created by Sunil Banjara on 22/02/14.
+//  Copyright (c) 2014 signity macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
